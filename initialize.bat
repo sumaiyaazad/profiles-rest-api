1c8344dd4227f4f5092ac64164a266d2fb571be0
sumaiyaazad <azadsumaiya00@gmail.com>
2021-10-31 21:13:07 +0600

    initialized django project

---
+++ initialize.bat
@@ -0,0 +1,14 @@
+@echo off "initializing project"
+python -m venv env
+@REM if you don't want to restart your ide run the following command otherwise ignore
+env\Scripts\activate.bat
+@REM deactive , for deactivating env
+pip install -r requirements.txt
+@REM create project folder in the root directory or else it will create a new project profiles_project folder
+django-admin startproject profiles_project .
+@REM python manage.py runserver 0.0.0.0:8000
+@REM python manage.py test
+@REM python manage.py startapp profiles_api
+@REM if we change the model we need to run the migrations again
+@REM python manage.py makemigrations core
+cd app
